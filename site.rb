bc9dff6316ffb881c0ea10daa92bfd89fc936f0d
Nat Welch <nat@natwelch.com>
2011-11-06 19:00:19 -0800

    We want json.

--- site.rb
+++ site.rb
@@ -65,7 +65,7 @@ get '/authed' do
   @access_token = @request_token.get_access_token
 
   begin
-    response = @access_token.get("http://api.twitter.com/1/users/lookup.format")
+    response = @access_token.get("http://api.twitter.com/1/users/lookup.json")
     return response.inspect
 
     user = JSON.parse(response)
