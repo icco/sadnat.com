ce0e18a3ba58259ff156b3ba51823768d0112942
Nat Welch <nat@natwelch.com>
2012-02-13 00:07:58 -0800

    link twitter handles.

--- site.rb
+++ site.rb
@@ -21,7 +21,16 @@ helpers do
 
   # To help us not dump scary stuff, but still autolink links.
   def h text
-    Sanitize.clean(text).gsub( %r{http(s)?://[^\s<]+} ) { |url| "<a href='#{url}'>#{url}</a>" }
+    # Clean out html
+    out = Sanitize.clean text
+
+    # Link links
+    out = out.gsub( %r{http(s)?://[^\s<]+} ) { |url| "<a href='#{url}'>#{url}</a>" }
+
+    # Link Twitter Handles
+    out = out.gsub(/@(\w+)/) {|a| "<a href=\"http://twitter.com/#{a[1..-1]}\"/>#{a}</a>" }
+
+    return out
   end
 end
 
