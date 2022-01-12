643b957d475b82209b7cf02a42efe5ff2c4080d6
Nat Welch <nat@natwelch.com>
2022-01-12 20:58:29 +0000

    delete

--- test/models/entry_test.rb
+++
@@ -1,8 +0,0 @@
-require File.expand_path(File.dirname(__FILE__) + '/../test_config.rb')
-
-describe "Entry Model" do
-  it 'can construct a new instance' do
-    @entry = Entry.new
-    refute_nil @entry
-  end
-end
